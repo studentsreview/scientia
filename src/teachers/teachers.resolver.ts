72c8b94b9d896e5bfad7e4197918e2b80092387d
Kai Chang <kaijchang@gmail.com>
2021-03-17 21:41:42 -0700

    handle 0 reviews case

--- src/teachers/teachers.resolver.ts
+++ src/teachers/teachers.resolver.ts
@@ -61,6 +61,7 @@ export class TeachersResolver {
       },
       {},
     );
+    if (reviews.length === 0) return 0;
     return reviews.reduce((acc, cur) => acc + cur.rating, 0) / reviews.length;
   }
 }
